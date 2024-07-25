ed4ede6df9bb87d78e30c9d252973cfcf5da111a
Lee Sihyeong <poeticform@10-141-55-243.0200.pos.kr.iosda.org>
2024-07-25 17:49:13 +0900

    add proj

---
+++ TeamLongHair/TeamLongHair/TeamLongHairApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamLongHairApp.swift
+//  TeamLongHair
+//
+//  Created by Lee Sihyeong on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TeamLongHairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
