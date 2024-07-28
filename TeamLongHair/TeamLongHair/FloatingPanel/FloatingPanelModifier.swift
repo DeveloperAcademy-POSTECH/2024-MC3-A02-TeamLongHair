6e115244de31cdf9c835e0c9ea1dc001619ab961
daminoworld <ekadls757@gmail.com>
2024-07-28 16:15:39 +0900

    feat: #3 Implement FloatingPanelModifier
    - FloatingPanel을 SwiftUI view 계층에 추가 하기 위한 modifier 구현
    - @ViewBuilder를 사용하여 패널의 뷰 클로저를 가지고 있게함
    - onAppear, onDisappear, onChange로 panel의 라이프 라이클 관리
    - present 함수를 통해 패널 키 윈도우로 설정

---
+++ TeamLongHair/TeamLongHair/FloatingPanel/FloatingPanelModifier.swift
@@ -0,0 +1,44 @@
+//
+//  FloatingPanelModifier.swift
+//  TeamLongHair
+//
+//  Created by Damin on 7/28/24.
+//
+
+import SwiftUI
+ 
+struct FloatingPanelModifier<PanelContent: View>: ViewModifier {
+    @Binding var isPresented: Bool
+ 
+    var contentRect: CGRect = CGRect(x: 0, y: 0, width: 624, height: 512)
+ 
+    @ViewBuilder let view: () -> PanelContent
+ 
+    @State var panel: FloatingPanel<PanelContent>?
+ 
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                panel = FloatingPanel(view: view, contentRect: contentRect, isPresented: $isPresented)
+                panel?.center()
+                if isPresented {
+                    present()
+                }
+            }.onDisappear {
+                panel?.close()
+                panel = nil
+            }.onChange(of: isPresented) { _, value in
+                if value {
+                    present()
+                } else {
+                    panel?.close()
+                }
+            }
+    }
+ 
+    func present() {
+        panel?.orderFront(nil)
+        panel?.makeKey()
+    }
+}
+
