c8272ddc33f02d0b06fbeccf02dc379970558318
daminoworld <ekadls757@gmail.com>
2024-07-28 16:24:31 +0900

    chore: #3 Add floatingPanel modifier method
    - FloatingPanelModifer를 뷰에서 modifer로 사용하기 위해 View 익스텐션에 floatingPanel을 정의하여 self.modifier를 구현

--- TeamLongHair/TeamLongHair/Extensions/View+.swift
+++ TeamLongHair/TeamLongHair/Extensions/View+.swift
@@ -8,5 +8,9 @@
 import SwiftUI
 
 extension View {
-    
+    func floatingPanel<Content: View>(isPresented: Binding<Bool>,
+                                      contentRect: CGRect = CGRect(x: 0, y: 0, width: 624, height: 512),
+                                      @ViewBuilder content: @escaping () -> Content) -> some View {
+        self.modifier(FloatingPanelModifier(isPresented: isPresented, contentRect: contentRect, view: content))
+    }
 }
