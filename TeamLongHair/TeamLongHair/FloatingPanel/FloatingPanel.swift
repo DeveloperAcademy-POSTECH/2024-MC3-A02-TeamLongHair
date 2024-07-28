fefc16484bfe7edd29d04a9bf1d34c871618d2b7
daminoworld <ekadls757@gmail.com>
2024-07-28 15:59:16 +0900

    feat: #3 Implement FloatingPanel with custom behaviors
    - NSPanel을 상속하는 FloatingPanel 생성
    - Configure properties for floating, hiding on deactivate, and custom title
    - NSPanel의 프로퍼티 설정: floating, hide, 사이즈 조절, 애니메이션 설정 등
    - NSHosingView를 이용해 SwiftUIView로 호스팅
    - 키 윈도우, 메인 윈도우 프로퍼티 오버라이드 설정
    - FloatingPanelKey를 만들어 environment 값으로 FloatingPanel이라는 NSPanel 서브클래스를 환경변수로 넘겨줌

--- TeamLongHair/TeamLongHair/FloatingPanel/FloatingPanel.swift
+++ TeamLongHair/TeamLongHair/FloatingPanel/FloatingPanel.swift
@@ -5,4 +5,74 @@
 //  Created by Damin on 7/28/24.
 //
 
-import Foundation
+import SwiftUI
+ 
+class FloatingPanel<Content: View>: NSPanel {
+    @Binding var isPresented: Bool
+    
+    init(view: () -> Content,
+             contentRect: NSRect,
+             backing: NSWindow.BackingStoreType = .buffered,
+             defer flag: Bool = false,
+             isPresented: Binding<Bool>) {
+        
+        self._isPresented = isPresented
+
+        super.init(contentRect: contentRect,
+                   styleMask: [.titled, .resizable],
+                    backing: backing,
+                    defer: flag)
+     
+        isFloatingPanel = true
+        level = .floating
+        
+        collectionBehavior.insert(.fullScreenAuxiliary)
+     
+        title = "URL을 붙여넣으세요"
+     
+        isMovableByWindowBackground = true
+     
+        hidesOnDeactivate = true
+     
+        standardWindowButton(.closeButton)?.isHidden = true
+        standardWindowButton(.miniaturizeButton)?.isHidden = true
+        standardWindowButton(.zoomButton)?.isHidden = true
+     
+        animationBehavior = .utilityWindow
+     
+        contentView = NSHostingView(rootView: view()
+            .ignoresSafeArea()
+            .environment(\.floatingPanel, self)
+        )
+    }
+    
+    override func resignMain() {
+        super.resignMain()
+        close()
+    }
+     
+    override func close() {
+        super.close()
+        isPresented = false
+    }
+     
+    override var canBecomeKey: Bool {
+        return true
+    }
+     
+    override var canBecomeMain: Bool {
+        return true
+    }
+}
+
+private struct FloatingPanelKey: EnvironmentKey {
+    static let defaultValue: NSPanel? = nil
+}
+ 
+extension EnvironmentValues {
+  var floatingPanel: NSPanel? {
+    get { self[FloatingPanelKey.self] }
+    set { self[FloatingPanelKey.self] = newValue }
+  }
+}
+
