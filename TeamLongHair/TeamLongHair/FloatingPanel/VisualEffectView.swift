aa3464ffc0dc970b154b2dc380391ac1f435d473
daminoworld <ekadls757@gmail.com>
2024-07-28 16:31:00 +0900

    feat: #3 Create VisualEffectView to intergrate NSVisualEffectView with SwiftUI
    - NSViewRepresentable를 이용해서 NSVisualEffectView 를 컨버팅 하는 VisualEffectView 구현
    - material, blendingMode, state, and emphasized에 대한 프로퍼티 정의
    - NSVisualEffectView의 업데이트 관리를 위해 Coordinator 클래스 생성

---
+++ TeamLongHair/TeamLongHair/FloatingPanel/VisualEffectView.swift
@@ -0,0 +1,49 @@
+//
+//  VisualEffectView.swift
+//  TeamLongHair
+//
+//  Created by Damin on 7/28/24.
+//
+
+import SwiftUI
+
+struct VisualEffectView: NSViewRepresentable {
+    var material: NSVisualEffectView.Material = .fullScreenUI
+    var blendingMode: NSVisualEffectView.BlendingMode = .behindWindow
+    var state: NSVisualEffectView.State = .followsWindowActiveState
+    var emphasized: Bool = false
+
+    func makeNSView(context: Context) -> NSVisualEffectView {
+        context.coordinator.visualEffectView
+    }
+
+    func updateNSView(_ view: NSVisualEffectView, context: Context) {
+        context.coordinator.update(
+            material: material,
+            blendingMode: blendingMode,
+            state: state,
+            emphasized: emphasized
+        )
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator {
+        let visualEffectView = NSVisualEffectView()
+
+        init() {
+        }
+
+        func update(material: NSVisualEffectView.Material,
+                    blendingMode: NSVisualEffectView.BlendingMode,
+                    state: NSVisualEffectView.State,
+                    emphasized: Bool) {
+            visualEffectView.material = material
+            visualEffectView.blendingMode = blendingMode
+            visualEffectView.state = state
+            visualEffectView.isEmphasized = emphasized
+        }
+    }
+}
