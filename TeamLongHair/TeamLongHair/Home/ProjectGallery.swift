5b03f792bfcd99cd73b34f63389dafac826c673f
dbqls200 <dbqls200@gmail.com>
2024-07-31 00:29:34 +0900

    chore: #9 Add right-click menu for project with delete and edit options
    - 우클릭으로 프로젝트 삭제 및 수정
    - 삭제 기능은 구현되어 있으나 추후 수정 기능 추가 작업 필요

--- TeamLongHair/TeamLongHair/Home/ProjectGallery.swift
+++ TeamLongHair/TeamLongHair/Home/ProjectGallery.swift
@@ -9,21 +9,34 @@ import SwiftUI
 
 struct ProjectGallery: View {
     var projects: [Project]
-    
+    let deleteProject: (Project) -> Void
+
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 280))], spacing: 20) {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 320))]) {
                 ForEach(projects) { project in
                     NavigationLink {
+                        // TODO: 페이지 + 캔버스 뷰
                         Text("page view")
                     } label: {
-                        VStack(alignment: .leading, spacing: 0) {
-                            RoundedRectangle(cornerRadius: 12)
-                                .frame(height: 200)
-                                .foregroundStyle(.clear)
-                                .padding(.bottom, 8)
+                        VStack(alignment: .leading, spacing: 8) {
+                            // TODO: 색상? 이미지? 변경
+                            RoundedRectangle(cornerRadius: 8)
+                                .frame(height: 220)
                             
                             Text(project.title)
+                        }
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 12, bottom: 48, trailing: 12))
+                    .contextMenu {
+                        Button("Delete") {
+                            deleteProject(project)
+                        }
+                        .keyboardShortcut(.delete)
+                        
+                        Button("Edit") {
+                            // TODO: 프로젝트 타이틀 Edit 작업 추가
+                            print("Edit")
                         }
                     }
                 }
