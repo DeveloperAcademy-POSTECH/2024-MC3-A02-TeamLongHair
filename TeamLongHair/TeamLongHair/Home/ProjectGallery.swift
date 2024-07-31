c7c8c90120cf73b6c39551fab3dc7fa8342df349
dbqls200 <dbqls200@gmail.com>
2024-07-31 16:05:39 +0900

    chore: #9 Sort projects by recent edit date and update the date on entry
    - 홈 화면에서 최근편집순으로 프로젝트 정렬
    - 프로젝트에 진입할 때 당일 날짜로 lastEditDate 값 update

--- TeamLongHair/TeamLongHair/Home/ProjectGallery.swift
+++ TeamLongHair/TeamLongHair/Home/ProjectGallery.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ProjectGallery: View {
     var projects: [Project]
     let deleteProject: (Project) -> Void
+    let updateEditDate: (Project) -> Void
 
     var body: some View {
         ScrollView {
@@ -25,8 +26,14 @@ struct ProjectGallery: View {
                                 .frame(height: 220)
                             
                             Text(project.title)
+                            
+                            Text(daysSinceLastEdit(project.lastEditDate))
+                        }
+                        .onTapGesture {
+                            updateEditDate(project)
                         }
                     }
+                    .buttonStyle(projectButtonStyle())
                     .padding(EdgeInsets(top: 0, leading: 12, bottom: 48, trailing: 12))
                     .contextMenu {
                         Button("Delete") {
@@ -35,7 +42,7 @@ struct ProjectGallery: View {
                         .keyboardShortcut(.delete)
                         
                         Button("Edit") {
-                            // TODO: 프로젝트 타이틀 Edit 작업 추가
+                            // TODO: 타이틀 수정 기능 추가하기
                             print("Edit")
                         }
                     }
@@ -43,5 +50,23 @@ struct ProjectGallery: View {
             }
             .padding()
         }
+    }
+    
+    private func daysSinceLastEdit(_ lastEditDate: Date) -> String {
+        let calendar = Calendar.current
+        let now = Date()
+        let components = calendar.dateComponents([.day], from: lastEditDate, to: now)
+        
+        if let days = components.day {
+            return "\(days)일 전 편집"
+        } else {
+            return "날짜 계산 오류"
+        }
+    }
+}
+
+struct projectButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
     }
 }
