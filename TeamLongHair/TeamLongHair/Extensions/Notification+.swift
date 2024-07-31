12111ba917961f768531ef4a8a185d12ada65c5c
daminoworld <ekadls757@gmail.com>
2024-07-31 16:29:55 +0900

    add: #5 Add Notification+ file
    - FloatingPanel에 대한 toggle notification의 이름 지정

---
+++ TeamLongHair/TeamLongHair/Extensions/Notification+.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+.swift
+//  TeamLongHair
+//
+//  Created by Damin on 7/31/24.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let toggleFloatingPanel = Notification.Name("toggleFloatingPanel")
+}
