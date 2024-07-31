966f0de4d93f56bf236bf53f843f84a1781e223f
daminoworld <ekadls757@gmail.com>
2024-07-31 22:36:49 +0900

    chore: #5 Change isPresented binding with appState value
    - 환경변수로 주입한 appState의 isPanelPresented 값으로 floatingPanel present 바인딩값 변경

--- TeamLongHair/TeamLongHair/FloatingPanel/FloatingPanelView.swift
+++ TeamLongHair/TeamLongHair/FloatingPanel/FloatingPanelView.swift
@@ -8,29 +8,32 @@
 import SwiftUI
 
 struct FloatingPanelView: View {
-    @State private var showingPanel = false
+    @Environment(AppState.self) var appState: AppState
     @State private var panelTitleText = ""
     @State private var panelURLText = ""
+
     var body: some View {
+        @Bindable var appState = appState
+        
         Button("Present panel") {
-                    showingPanel.toggle()
+            appState.isPanelPresented.toggle()
+        }
+        .floatingPanel(isPresented: $appState.isPanelPresented) {
+            FloatingPanelExpandableLayout(toolbar: {
+                VStack {
+                    TextField(text: $panelURLText) {
+                        Text("URL")
+                    }
+                    TextField(text: $panelTitleText) {
+                        Text("Title")
+                    }
                 }
-                .floatingPanel(isPresented: $showingPanel) {
-                    FloatingPanelExpandableLayout(toolbar: {
-                        VStack {
-                            TextField(text: $panelURLText) {
-                                Text("URL")
-                            }
-                            TextField(text: $panelTitleText) {
-                                Text("Title")
-                            }
-                        }
-                    }, projectBar: {
-                        Text("ProjectList")
-                    }, pageBar: {
-                        Text("PageList")
-                    })
-                }
+            }, projectBar: {
+                Text("ProjectList")
+            }, pageBar: {
+                Text("PageList")
+            })
+        }
     }
 }
 
