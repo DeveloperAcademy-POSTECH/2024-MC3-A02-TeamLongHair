0aa934bbdb54c5592aa8a4268916a68b332b48c4
daminoworld <ekadls757@gmail.com>
2024-07-31 17:41:53 +0900

    feat: #5 Add shortcutSettingView
    - 단축키 설정을 위한 뷰인 ShortcutSettingsView 추가
    - UserDefaults에서 저장된 단축키 값을 불러와 초기화하는 로직 구현
    - 사용자가 단축키를 입력할 수 있도록 키 이벤트 모니터링을 시작하는 startListeningForShortcut() 메서드 추가
    - 단축키를 저장하고 UserDefaults와 AppState에 반영하는 saveShortcut() 메서드 구현
    - 뷰가 사라질 때 키 이벤트 모니터링을 중지하는 stopMonitoringKeys() 메서드 추가

---
+++ TeamLongHair/TeamLongHair/FloatingPanel/ShortcutSettingView.swift
@@ -0,0 +1,74 @@
+//
+//  ShortcutSettingView.swift
+//  TeamLongHair
+//
+//  Created by Damin on 7/31/24.
+//
+
+import SwiftUI
+
+struct ShortcutSettingsView: View {
+    @Environment(AppState.self) var appState: AppState
+    @State private var shortcut: KeyShortcut?
+    @State private var localKeyMonitor: Any?
+    
+    init() {
+        // UserDefaults에서 저장된 값을 불러와서 초기화
+        let keyCode = UserDefaults.standard.integer(forKey: "shortcutKeyCode")
+        let modifierFlagsValue = UserDefaults.standard.integer(forKey: "shortcutModifierFlags")
+        let modifierFlags = NSEvent.ModifierFlags(rawValue: UInt(modifierFlagsValue))
+        
+        // shortcut State 변수의 초기값 설정
+        if keyCode != 0 || modifierFlagsValue != 0 {
+            _shortcut = State(initialValue: KeyShortcut(modifierFlags: modifierFlags, keyCode: keyCode))
+        } else {
+            _shortcut = State(initialValue: nil)
+        }
+    }
+
+    var body: some View {
+        VStack {
+            Text("Press your desired shortcut")
+            Text(shortcut?.description ?? "No shortcut set")
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+            Button("Save Shortcut") {
+                saveShortcut()
+            }
+            .disabled(shortcut == nil)
+        }
+        .padding()
+        .onAppear {
+            startListeningForShortcut()
+        }
+        .onDisappear {
+            stopMonitoringKeys()
+        }
+    }
+
+    func startListeningForShortcut() {
+        localKeyMonitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
+            shortcut = .init(modifierFlags: event.modifierFlags, keyCode: Int(event.keyCode))
+            return event
+        }
+    }
+
+    func saveShortcut() {
+        if let shortcut = shortcut {
+            UserDefaults.standard.set(shortcut.keyCode, forKey: "shortcutKeyCode")
+            UserDefaults.standard.set(shortcut.modifierFlags.rawValue, forKey: "shortcutModifierFlags")
+            
+            appState.keyCode = shortcut.keyCode
+            appState.modifierFlags = shortcut.modifierFlags
+        }
+        stopMonitoringKeys()
+    }
+    
+    func stopMonitoringKeys() {
+        if let localKeyMonitor = self.localKeyMonitor {
+            NSEvent.removeMonitor(localKeyMonitor)
+            self.localKeyMonitor = nil
+        }
+    }
+}
