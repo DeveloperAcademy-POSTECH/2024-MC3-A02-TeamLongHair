ab8d3ab81710efdcc33b0fd6a55865e1816f9f82
dbqls200 <dbqls200@gmail.com>
2024-08-05 02:10:28 +0900

    chore: #16 Add sidebar for Link Panel
    - 좌측에 Link Panel을 띄우기 위해 NavigationSplitView를 사용하여 sidebar 추가
    - title 부분만 작성했고 이후 Pages/Links 리스트 작업할 예정
    - 최근편집순으로 정렬하기 위해, 프로젝트에 진입했을 때 날짜 update

--- TeamLongHair/TeamLongHair/Project/ProjectView.swift
+++ TeamLongHair/TeamLongHair/Project/ProjectView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct ProjectView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    var project: Project
+    
+    @State var page: Page
+    
+    @State private var isShowingTextField = false
+    
+    init(project: Project) {
+        self.project = project
+        self.page = self.project.pages[0]
     }
-}
-
-#Preview {
-    ProjectView()
+    
+    var body: some View {
+        NavigationSplitView {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(spacing: 8) {
+                    Text("\(project.title)")
+                        .font(.system(size: 16))
+                        .foregroundStyle(.lbPrimary)
+                        .lineLimit(1)
+                    
+                    Button {
+                        isShowingTextField.toggle()
+                    } label: {
+                        Image(systemName: "chevron.down")
+                    }
+                    .buttonStyle(defaultButtonStyle())
+                }
+                .padding(20)
+                
+                // TODO: 프로젝트 타이틀 입력받을 text field + update 로직 넣기
+            }
+            .background(.white)
+            .frame(minWidth: 300)
+        } detail: {
+            CanvasView()
+        }
+        .onAppear {
+            updateProjectLastEditDate(project)
+        }
+    }
+    
+    private func updateProjectLastEditDate(_ project: Project) {
+        project.lastEditDate = Date.now
+    }
 }
