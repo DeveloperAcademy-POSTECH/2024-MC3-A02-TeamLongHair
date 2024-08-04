b8473cd5f03a386c088217c0847e822e79bd9fac
daminoworld <ekadls757@gmail.com>
2024-08-04 15:37:05 +0900

    refactor: #19 Remove keyShortcut handling logic from appDelegate
    - AppDelegate에서 키보드 단축키 이벤트에 따른 처리 로직을 AppState로 옮기기 위해 AppDelegate에서 삭제

--- TeamLongHair/TeamLongHair/Utils/AppDelegate.swift
+++ TeamLongHair/TeamLongHair/Utils/AppDelegate.swift
@@ -12,64 +12,25 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     private let appState = AppState.shared
     private var localKeyMonitor: Any?
     private var globalKeyMonitor: Any?
-    // 디폴트 단축키 정보
-    private let defaultKeyCode: Int = 5 // G 키
-    private let defaultModifierFlags: NSEvent.ModifierFlags = [.command, .shift]
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         requestAccessibilityPermissions()
-        loadAndStartMonitoringKeys()
+        startMonitoringKeys()
     }
     
     func applicationWillTerminate(_ notification: Notification) {
         stopMonitoringKeys()
     }
     
-    func loadAndStartMonitoringKeys() {
-        let userDefaults = UserDefaults.standard
-
-        // 기본값을 UserDefaults에 저장
-        guard let savedKeyCode = userDefaults.object(forKey: "shortcutKeyCode") as? Int else {
-            userDefaults.set(defaultKeyCode, forKey: "shortcutKeyCode")
-            appState.keyCode = defaultKeyCode
-            return
-        }
-        
-        guard let savedModifierFlagsValue = userDefaults.object(forKey: "shortcutModifierFlags") as? UInt else {
-            userDefaults.set(defaultModifierFlags.rawValue, forKey: "shortcutModifierFlags")
-            appState.modifierFlags = defaultModifierFlags
-            return
-        }
-
-        appState.keyCode = savedKeyCode
-        appState.modifierFlags = NSEvent.ModifierFlags(rawValue: savedModifierFlagsValue)
-        
-        startMonitoringKeys()
-    }
-    
     func startMonitoringKeys() {
         // 로컬 키 모니터링
         localKeyMonitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
-            // ESC 키코드
-            if event.keyCode == 53 {
-                NotificationCenter.default.post(name: .toggleFloatingPanel, object: nil, userInfo: ["shouldPresent": false])
-                return event
-            }
-            
-            if event.keyCode == self.appState.keyCode ?? 0 && event.modifierFlags.intersection(.deviceIndependentFlagsMask) == self.appState.modifierFlags?.intersection(.deviceIndependentFlagsMask) ?? [] {
-                debugPrint("Custom Local shortcut triggered")
-                NotificationCenter.default.post(name: .toggleFloatingPanel, object: nil)
-                return event
-            }
-            return event
+            self.appState.checkLocalEventIsKeyShortcut(event: event)
         }
         
         // 글로벌 키 모니터링
-        globalKeyMonitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { [self] event in
-            if event.keyCode == self.appState.keyCode ?? 0 && event.modifierFlags.intersection(.deviceIndependentFlagsMask) == appState.modifierFlags?.intersection(.deviceIndependentFlagsMask) ?? [] {
-                debugPrint("Custom Global shortcut triggered")
-                NotificationCenter.default.post(name: .toggleFloatingPanel, object: nil)
-            }
+        globalKeyMonitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { event in
+            self.appState.checkGlobalEventIsKeyShortcut(event: event)
         }
     }
     
