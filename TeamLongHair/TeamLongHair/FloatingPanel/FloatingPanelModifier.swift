121073327a4f062439b2b77f40e63712135f0414
daminoworld <ekadls757@gmail.com>
2024-08-04 16:01:34 +0900

    chore: #19 Remove empty llines

--- TeamLongHair/TeamLongHair/FloatingPanel/FloatingPanelModifier.swift
+++ TeamLongHair/TeamLongHair/FloatingPanel/FloatingPanelModifier.swift
@@ -9,13 +9,10 @@ import SwiftUI
  
 struct FloatingPanelModifier<PanelContent: View>: ViewModifier {
     @Binding var isPresented: Bool
- 
     var contentRect: CGRect = CGRect(x: 0, y: 0, width: 624, height: 512)
- 
     @ViewBuilder let view: () -> PanelContent
- 
     @State var panel: FloatingPanel<PanelContent>?
- 
+
     func body(content: Content) -> some View {
         content
             .onAppear {
@@ -34,7 +31,7 @@ struct FloatingPanelModifier<PanelContent: View>: ViewModifier {
                 }
             }
     }
- 
+    
     func present() {
         NSApp.activate(ignoringOtherApps: true)
         panel?.center()
