acb16185f849dd28803c6756a82eda0bba625d38
chosun-sangnom <kj82076@gmail.com>
2024-08-07 03:10:45 +0900

    feat: #31 Applied node color and Add Copy button action in CodeBlockView - 하는김에 최적화도 했습니다.

--- TeamLongHair/TeamLongHair/RightPanel/RightPanelView.swift
+++ TeamLongHair/TeamLongHair/RightPanel/RightPanelView.swift
@@ -8,24 +8,41 @@
 import SwiftUI
 
 struct RightPanelView: View {
+    @State private var selectedColorIndex: IconColor? = nil
+    
+    var colors: [Color] = [
+        .gray,
+        Color("node_red"),
+        Color("node_orange"),
+        Color("node_yellow"),
+        Color("node_green"),
+        Color("node_sky"),
+        Color("node_blue"),
+        Color("node_purple"),
+        Color("node_plum")
+    ]
+    
     var body: some View {
         ScrollView{
             VStack{
-                LinkView()
+                LinkView(selectedColorIndex: $selectedColorIndex, colors: colors)
                 Divider()
                 TagView()
                 Divider()
-                ColorView()
+                ColorView(selectedColorIndex: $selectedColorIndex, colors: colors)
                 Divider()
                 MemoView()
                 Divider()
                 CodeBlockView()
             }
-        }.frame(width: 300,height: 834)
-            .background(Color.bgPrimary)
+        }
+        .frame(width: 300, height: 834)
+        .background(Color.bgPrimary)
     }
 }
 
-#Preview {
-    RightPanelView()
+struct RightPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        RightPanelView()
+    }
 }
